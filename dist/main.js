d5e0b2673a1d5cc6a763ef1b25c37064f7b2a82b
David <david.kushnir.d@gmail.com>
2023-04-02 23:23:46 +0300

    cors

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,9 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: 'https://dietolog-2-0-client-git-dietolog-20-front-gallahar.vercel.app',
+    });
     app.setGlobalPrefix('api');
     await app.listen(5000);
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	app.enableCors({
+		origin: 'https://dietolog-2-0-client-git-dietolog-20-front-gallahar.vercel.app',
+	})
 	app.setGlobalPrefix('api')
 	await app.listen(5000)
 }

b8fbf604ebf37bad62c5fc6116f5a1f799a8853c
David <david.kushnir.d@gmail.com>
2023-04-06 20:15:16 +0300

    cors change

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.enableCors({
-		origin: 'https://dietolog-2-0-client-hyqjk2no9-gallahar.vercel.app',
+		origin: 'https://dietolog-2-0-client-fspccclh3-gallahar.vercel.app',
 	})
 	app.setGlobalPrefix('api')
 	await app.listen(5000)
